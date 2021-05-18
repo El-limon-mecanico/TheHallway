8a2f644e4440acebe566551ee39959edf6a46918
Jorge <jorgbell@ucm.es>
2021-05-18 22:33:00 +0200

    Componente Lighter creado en escena, problemas con la sceneLight para visualizar bien la linterna

--- TheHallwaySol/Assets/lua/Scenes/scene1.lua
+++ TheHallwaySol/Assets/lua/Scenes/scene1.lua
@@ -1,19 +1,21 @@
 ﻿scene1 = {
-    entities = {"defaultCamera", "sceneLight", "Maze"}
+    entities = {"defaultCamera","player","sceneLight","Maze"}
 }
 
 defaultCamera = {
+    Active = true,
+
     Components = {"Transform", "Camera"},
 
     Transform = {
-        Position = {25, 75,25},
+        Position = {25, 80,25},
         Scale = {1,1,1},
         Rotation = {0,0,0}
     },
 
     Camera = {
         Name = "MainCam",
-        Background = {1,1,0},
+        Background = {0,0,0},
         LookAt = {25,0,25},
         Width = 0,
         Height = 0,
@@ -24,6 +26,7 @@ defaultCamera = {
 }
 
 sceneLight = {
+    Active = true,
     Components = {"Transform", "Light"},
 
     Transform = {
@@ -37,14 +40,42 @@ sceneLight = {
         DiffuseColor = {1,1,1},
         SpecularColor = {1,1,1},
         Direction = {0,0,0},
-        Distance = 100,
-        InnerAngle = 30,
-        OuterAngle = 90,
+        Distance = 50,
+        InnerAngle = 20,
+        OuterAngle = 60,
         isOn = true
     }
 }
 
+player = {
+    Active = true,
+    Components = {"Transform", "Light", "Lighter"},
+
+    Transform = {
+        Position = {0,0,0},
+        Scale = {1,1,1},
+        Rotation = {0,0,0}
+    },
+
+    Light = {
+        LightType = 2,
+        DiffuseColor = {1,1,1},
+        SpecularColor = {1,1,1},
+        Direction = {0,0,0},
+        Distance = 2,
+        InnerAngle = 20,
+        OuterAngle = 60,
+        isOn = true
+    },
+    Lighter ={
+        ChargeSpeed = 0.01,
+        DischargeSpeed = 0.03
+    }
+
+}
+
 Maze = {
+    Active = true,
     Components = {"Transform", "MazeCreator"},
 
     Transform = {
@@ -53,7 +84,7 @@ Maze = {
         Rotation = {0,0,0}
     },
     MazeCreator = {
-        Size = 25,
-        Holes = 25
+        Size = 15,
+        Holes = 5
     }
 }
--- TheHallwaySol/Src/TheHallway/Lighter.cpp
+++ TheHallwaySol/Src/TheHallway/Lighter.cpp
@@ -1,17 +1,49 @@
 #include "Lighter.h"
-#include "Transform.h"
+#include "Light.h"
+#include "InputManager.h"
+#include "QuackEntity.h"
+#include "QuackEnginePro.h"
+#include "PlayerMovement.h"
+
 
 bool Lighter::init(luabridge::LuaRef parameterTable)
 {
+	chargeSpeed_ = readVariable<float>(parameterTable, "ChargeSpeed");
+	dischargeSpeed_ = readVariable<float>(parameterTable, "DischargeSpeed");
 	return true;
 }
 
-void Lighter::charge()
-{
 
+void Lighter::update() {
+	// si la linterna no está apagada y se pulsa el botón de carga, se carga la linterna     
+	if (InputManager::Instance()->getMouseButtons().leftDown) {
+		//cargas si se puede hacer
+		if (actualCharge_ < 1) {
+			//desactivas el movimiento
+			/*if (pM_->isEnable())
+				pM_->setEnable(false);*/
+			actualCharge_ += QuackEnginePro::Instance()->time()->deltaTime() * chargeSpeed_;
+			if (actualCharge_ > 1) //si llega al máximo, setea
+				actualCharge_ = 1;
+		}
+	}
+	else {
+		//activa el movimiento si fuese necesario
+		/*if (!pM_->isEnable())
+			pM_->setEnable(true);*/
+		//descargas
+		if (actualCharge_ >0) {
+			actualCharge_ -= QuackEnginePro::Instance()->time()->deltaTime() * dischargeSpeed_;
+			if (actualCharge_ < 0)//si llega al mínimo, setea
+				actualCharge_ = 0;
+		}
+	}
+	light_->setDistance(1000 * actualCharge_);
+	std::cout << "CARGA DE LA LINTERNA: " << actualCharge_ * 100 << "%\n";
 }
 
-void Lighter::update()
+void Lighter::start()
 {
-
+	light_ = entity_->getComponent<Light>();
+	pM_ = entity_->getComponent<PlayerMovement>();
 }
