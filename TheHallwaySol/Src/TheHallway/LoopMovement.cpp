8074c6cd1721b834821f871d463b9d64cf1bff02
anmartan <anmartan@ucm.es>
2021-05-19 20:22:23 +0200

    ADDED: ChasePlayer
    Persigue al jugador y vuelve a merodear si el jugador se aleja.
    Pequeños cambios en el comportamiento del circuito.
    Escena con un jugador Player

--- TheHallwaySol/Src/TheHallway/LoopMovement.cpp
+++ TheHallwaySol/Src/TheHallway/LoopMovement.cpp
@@ -2,12 +2,16 @@
 #include "Transform.h"
 #include "QuackEntity.h"
 #include "Rigidbody.h"
+#include "SceneMng.h"
+#include "Scene.h"
+#include "ChasePlayer.h"
+
 bool LoopMovement::init(luabridge::LuaRef parameterTable)
 {
 	LuaRef speed = readVariable<LuaRef>(parameterTable, "Speed");
+	playerTriggerDistance_ = readVariable<LuaRef>(parameterTable, "Distance");
 	speed_ = speed;
 	LuaRef ob = readVariable<LuaRef>(parameterTable, "Objectives");
-	std::cout << ob.length() << "\n";
 	for (size_t i = 1; i <= ob.length(); i++)
 	{
 		Vector3D v;
@@ -35,10 +39,13 @@ void LoopMovement::updateObjective() {
 void LoopMovement::updateVelocity()
 {
 	assert(rb_);
+	transform->lookAt(objectives_[actualObjective_]);
 	rb_->setVelocity(transform->forward * speed_);
 }
 void LoopMovement::start()
 {
+	assert(SceneMng::Instance()->getCurrentScene()->getObjectWithName("Player"));
+	playerTr_ = SceneMng::Instance()->getCurrentScene()->getObjectWithName("Player")->transform();
 	assert(entity_->hasComponent<Rigidbody>());
 	rb_ = entity_->getComponent<Rigidbody>();
 	rb_->setGravity({ 0,0,0 });
@@ -47,4 +54,45 @@ void LoopMovement::start()
 }
 void LoopMovement::update() {
 	updateObjective();
+	// si estamos cerca del jugador, empezamos a perseguirle
+	float distance = (transform->position() - playerTr_->position()).magnitude();
+	std::cout << distance << "\n";
+	if (distance < playerTriggerDistance_)
+	{
+		setEnable(false);
+	}
 }
+
+/// <summary>
+/// busca el punto mas cercano del circuito. Desde ahi empieza el circuito
+/// </summary>
+void LoopMovement::onEnable()
+{
+	// buscamos el objetivo mas cercano para volver al circuito
+	float distanceMagn = INT_MAX;
+	int index = 0;
+	for (int i = 0; i < objectives_.size(); i++)
+	{
+		Vector3D distance = objectives_[i] - transform->position();
+		float auxDist = distance.magnitude();
+		if (auxDist < distanceMagn)
+		{
+			distanceMagn = auxDist;
+			index = i;
+		}
+	}
+
+	// cambiamos el objetivo por el que hemos calculado
+	actualObjective_ = index;
+	if(rb_)
+		updateVelocity();
+}
+
+void LoopMovement::onDisable()
+{
+	// volvemos al comportamiento de persecucion
+	// assert porque no tenemos otro movimiento para los enemigos
+	assert(entity_->hasComponent<ChasePlayer>());
+	entity_->getComponent<ChasePlayer>()->setEnable(true);
+}
+
--- TheHallwaySol/Src/TheHallway/Source.cpp
+++ TheHallwaySol/Src/TheHallway/Source.cpp
@@ -6,6 +6,8 @@
 #include "Lighter.h"
 #include "PlayerMovement.h"
 #include "LoopMovement.h"
+#include "ChasePlayer.h"
+
 #if (defined _DEBUG) //|| !(defined _WIN64)
 int main() {
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
@@ -21,6 +23,7 @@ WinMain(HINSTANCE zHInstance, HINSTANCE prevInstance, LPSTR lpCmdLine, int nCmdS
 		FactoryManager::instance()->add<MazeCreator>();
 		FactoryManager::instance()->add<Lighter>();
 		FactoryManager::instance()->add<PlayerMovement>();
+		FactoryManager::instance()->add<ChasePlayer>();
 		QuackEnginePro::Instance()->start("Scenes/scene1.lua", "scene1");
 	}
 	return 0;
